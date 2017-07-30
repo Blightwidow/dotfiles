6fec4e4311500d8fa0cecb521ea26fa041df4575
Blightwidow <theo@dammaretz.fr>
2017-07-30 21:46:34 +0300

    Add vscode

--- install.sh
+++ install.sh
@@ -3,16 +3,24 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-files="gitconfig conkyrc"    # list of files/folders to symlink in homedir
+files="gitconfig conkyrc "    # list of files/folders to symlink in homedir
+vscode="vscode"
 
 # change to the dotfiles directory
 echo -n "Changing to the $dir directory"
 cd $dir
 echo "	...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+# create symlinks
 for file in $files; do
     echo -n "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    echo "	...done"
+done
+
+# create simlink for visual studio
+for file in $files; do
+    echo -n "Creating symlink to $file in home directory."
+    ln -s $dir/$vscode ~/.config/Code/User/settings.json
     echo "	...done"
 done
