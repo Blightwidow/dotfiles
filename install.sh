c76a928cd5a9c9181f8dd9626ef8e314aee5d1fe
Blightwidow <theo@dammaretz.fr>
2017-07-30 21:38:25 +0300

    Removed atom

--- install.sh
+++ install.sh
@@ -4,21 +4,15 @@
 
 dir=~/dotfiles                    # dotfiles directory
 files="gitconfig conkyrc"    # list of files/folders to symlink in homedir
-atom="atom-packages"    # list of files/folders to symlink in homedir
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+echo -n "Changing to the $dir directory"
 cd $dir
-echo "...done"
+echo "	...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Creating symlink to $file in home directory."
+    echo -n "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
-done
-
-# atom setup
-for file in $atom; do
-    echo "Installing atom packages from $file"
-    apm install --packages-file $file
+    echo "	...done"
 done
